9377490ae33a9b4461b9262bd75323f0f7527e0e
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-11-18 21:00:05 +0100

    refactor: usize to i32 to avoid type casting
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- src/board.rs
+++ src/board.rs
@@ -32,6 +32,22 @@ impl Default for Board {
 
 impl Board {
 
+    // Getters
+
+    pub fn get_piece_type(&mut self, coordinates: [i32; 2]) -> char {
+        return self.board[coordinates[0] as usize][coordinates[1] as usize].chars().nth(1).unwrap();
+    }
+
+    pub fn get_piece_color(&mut self, coordinates: [i32; 2]) -> char {
+        return self.board[coordinates[0] as usize][coordinates[1] as usize].chars().next().unwrap();
+    }
+
+    pub fn authorized_positions_enum(&mut self, piece: char, color: char) -> Vec<Vec<i32>>{
+        match piece {
+            'P' => Pawn::authorized_positions(self.selected_coordinates, color),
+            _ => vec![]
+        }
+    }
     // Check if a cell has been selected
     fn is_cell_selected(&mut self) -> bool {
         self.selected_coordinates[0] != UNDEFINED_POSITION && self.selected_coordinates[1] != UNDEFINED_POSITION
@@ -84,7 +100,7 @@ impl Board {
             .split(area);
     
         // For each line we set 8 layout
-        for i in 0..8{
+        for i in 0..8i32{
             let lines = Layout::default()
             .direction(Direction::Horizontal)
             .constraints(
@@ -100,52 +116,69 @@ impl Board {
         
                 ]
                 .as_ref(),
-            ).split(columns[i]);
-            for j in 0..8{
+            ).split(columns[i as usize]);
+            for j in 0..8i32{
                 // Color of the cell to draw the board
-                let cell_color: Color = if (i + j) % 2 == 0 {
+                let mut cell_color: Color = if (i + j) % 2 == 0 {
                     Color::Rgb(210, 200, 190)
                 } else {
                     Color::Rgb(128, 95, 69)
                 };
+
+                // Draw the available moves for the selected piece
+                if self.is_cell_selected() {
+                    let selected_piece_type:char = self.get_piece_type(self.selected_coordinates);
+                    let selected_piece_color:char = self.get_piece_color(self.selected_coordinates);
+                    let positions = self.authorized_positions_enum(selected_piece_type, selected_piece_color);
+
+                    for coords in positions.clone(){
+                        if i == coords[0] && j == coords[1]{
+                            cell_color = Color::LightRed
+                        }
+                    }
+                }
+
+
                 // Draw the cell blue if this is the current cursor cell
-                if (i as i32 == self.cursor_y && j as i32 == self.cursor_x) && !self.is_cell_selected(){
+                if (i == self.cursor_y && j == self.cursor_x) && !self.is_cell_selected(){
                     let cell = Block::default().bg(Color::LightBlue).add_modifier(Modifier::RAPID_BLINK);
-                    frame.render_widget(cell.clone(),lines[j]); 
+                    frame.render_widget(cell.clone(),lines[j as usize]); 
                 }
                 // Draw the cell green if this is the selected cell
-                else if i as i32 == self.selected_coordinates[0] && j as i32 == self.selected_coordinates[1]{
+                else if i == self.selected_coordinates[0] && j == self.selected_coordinates[1]{
                     let cell = Block::default().bg(Color::LightGreen);
-                    frame.render_widget(cell.clone(),lines[j]); 
+                    frame.render_widget(cell.clone(),lines[j as usize]); 
                 } 
                 else{
                     let cell = Block::default().bg(cell_color);
-                    frame.render_widget(cell.clone(),lines[j]);
+                    frame.render_widget(cell.clone(),lines[j as usize]);
                 }
-                let piece_color = &self.board[i][j][0..1];
-                let piece_type = &self.board[i][j][1..2];
+
+
+
+                // Get piece and color
+                let piece_color = self.get_piece_color([i, j]);
+                let piece_type =self.get_piece_type([i, j]);
 
                 let color_enum = match piece_color {
-                    "b" => Color::Black,
-                    "w" => Color::White,
+                    'b' => Color::Black,
+                    'w' => Color::White,
                     _ => Color::Red
                 };
 
                 let piece_enum = match piece_type {
-                    "Q" => Queen::to_string(),
-                    "K" => King::to_string(),
-                    "R" => Rook::to_string(),
-                    "B" => Bishop::to_string(),
-                    "N" => Knight::to_string(),
-                    "P" => Pawn::to_string(),
+                    'Q' => Queen::to_string(),
+                    'K' => King::to_string(),
+                    'R' => Rook::to_string(),
+                    'B' => Bishop::to_string(),
+                    'N' => Knight::to_string(),
+                    'P' => Pawn::to_string(),
                     _ => "",
                 };
 
                 // Place the pieces on the board
                 let paragraph = Paragraph::new(piece_enum).alignment(Alignment::Center).fg(color_enum);
-                frame.render_widget(paragraph,lines[j]);
-
-                
+                frame.render_widget(paragraph,lines[j as usize]);
             }
         }
     }
