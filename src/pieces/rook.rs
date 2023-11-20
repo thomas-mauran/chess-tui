151f8b837719c54d25d394e9ca2bf156fdb3795e
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-11-20 23:23:25 +0100

    test(rook): add test
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- src/pieces/rook.rs
+++ src/pieces/rook.rs
@@ -126,3 +126,105 @@ impl Rook{
     cleaned_positions(positions)
   }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{board::Board, pieces::{PieceType, PieceColor, rook::Rook}};
+
+    #[test]
+    fn rook_moves_no_enemies() {
+        let custom_board = [
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, Some((PieceType::Rook, PieceColor::White)), None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+        ];
+        let mut board = Board::default();
+        board.set_board(custom_board);
+
+        let right_positions = vec![
+          vec![3, 4],
+          vec![2, 4],
+          vec![1, 4],
+          vec![0, 4],
+
+          vec![4, 5],
+          vec![4, 6],
+          vec![4, 7],
+
+          vec![5, 4], 
+          vec![6, 4],
+          vec![7, 4],
+        ].sort();
+
+        let positions = Rook::authorized_positions([4, 4], PieceColor::White, board.board).sort();
+
+        assert_eq!(right_positions, positions);
+    }
+
+    #[test]
+    fn rook_moves_one_enemies_front() {
+        let custom_board = [
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, Some((PieceType::Pawn, PieceColor::Black)), None, None, None],
+            [None, None, None, None, Some((PieceType::Rook, PieceColor::White)), None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+        ];
+        let mut board = Board::default();
+        board.set_board(custom_board);
+
+        let right_positions = vec![
+          vec![3, 4],
+
+          vec![4, 5],
+          vec![4, 6],
+          vec![4, 7],
+
+          vec![5, 4], 
+          vec![6, 4],
+          vec![7, 4],
+        ].sort();
+
+        let positions = Rook::authorized_positions([4, 4], PieceColor::White, board.board).sort();
+
+        assert_eq!(right_positions, positions);
+    }
+
+    #[test]
+    fn rook_moves_multiple_enemies_and_ally_front() {
+        let custom_board = [
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, Some((PieceType::Pawn, PieceColor::Black)), None, None, None],
+            [None, None, None, None, Some((PieceType::Rook, PieceColor::White)), None, Some((PieceType::Pawn, PieceColor::Black)), None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, Some((PieceType::Rook, PieceColor::White)), None, None],
+            [None, None, None, None, None, None, None, None],
+        ];
+        let mut board = Board::default();
+        board.set_board(custom_board);
+
+        let right_positions = vec![
+          vec![3, 4],
+
+          vec![4, 5],
+          vec![4, 6],
+
+          vec![5, 4], 
+        ].sort();
+
+        let positions = Rook::authorized_positions([4, 4], PieceColor::White, board.board).sort();
+
+        assert_eq!(right_positions, positions);
+    }
+}
