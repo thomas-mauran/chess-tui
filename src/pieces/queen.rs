cc81afaad557b5c4199897399abb082140c7e9e1
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-11-21 18:21:24 +0100

    test(queen): add tests
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- src/pieces/queen.rs
+++ src/pieces/queen.rs
@@ -25,3 +25,150 @@ impl Queen{
     cleaned_positions(positions)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::{board::Board, pieces::{PieceType, PieceColor, queen::Queen}};
+
+  #[test]
+  fn queen_moves_no_enemies() {
+      let custom_board = [
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, Some((PieceType::Queen, PieceColor::White)), None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+      ];
+      let mut board = Board::default();
+      board.set_board(custom_board);
+
+      let mut right_positions = vec![
+        vec![0, 0], 
+        vec![0, 4], 
+        vec![1, 1], 
+        vec![1, 4], 
+        vec![1, 7], 
+        vec![2, 2], 
+        vec![2, 4], 
+        vec![2, 6], 
+        vec![3, 3], 
+        vec![3, 4],  
+        vec![3, 5], 
+        vec![4, 0], 
+        vec![4, 1], 
+        vec![4, 2], 
+        vec![4, 3], 
+        vec![4, 5], 
+        vec![4, 6], 
+        vec![4, 7], 
+        vec![5, 3], 
+        vec![5, 4], 
+        vec![5, 5], 
+        vec![6, 2], 
+        vec![6, 4], 
+        vec![6, 6], 
+        vec![7, 1], 
+        vec![7, 4], 
+        vec![7, 7]];
+      right_positions.sort();
+
+      let mut positions = Queen::authorized_positions([4, 4], PieceColor::White, board.board);
+      positions.sort();
+
+      assert_eq!(right_positions, positions);
+  }
+
+  #[test]
+  fn queen_moves_one_enemies_top_right() {
+      let custom_board = [
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, Some((PieceType::Pawn, PieceColor::Black)), None, None],
+          [None, None, None, None, Some((PieceType::Queen, PieceColor::White)), None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+      ];
+      let mut board = Board::default();
+      board.set_board(custom_board);
+
+      let mut right_positions = vec![
+        vec![0, 0], 
+        vec![0, 4], 
+        vec![1, 1], 
+        vec![1, 4], 
+        vec![2, 2], 
+        vec![2, 4], 
+        vec![3, 3], 
+        vec![3, 4], 
+        vec![3, 5], 
+        vec![4, 0], 
+        vec![4, 1], 
+        vec![4, 2], 
+        vec![4, 3], 
+        vec![4, 5], 
+        vec![4, 6], 
+        vec![4, 7], 
+        vec![5, 3], 
+        vec![5, 4], 
+        vec![5, 5], 
+        vec![6, 2], 
+        vec![6, 4], 
+        vec![6, 6], 
+        vec![7, 1], 
+        vec![7, 4], 
+        vec![7, 7]];
+      right_positions.sort();
+
+      let mut positions = Queen::authorized_positions([4, 4], PieceColor::White, board.board);
+      positions.sort();
+
+      assert_eq!(right_positions, positions);
+  }
+
+  #[test]
+  fn queen_moves_enemies_and_allies() {
+      let custom_board = [
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, Some((PieceType::Pawn, PieceColor::Black)), Some((PieceType::Pawn, PieceColor::White)), None, None, None],
+        [None, Some((PieceType::Pawn, PieceColor::Black)), None, None, Some((PieceType::Queen, PieceColor::White)), None, None, None],
+        [None, None, None, None, None, Some((PieceType::Pawn, PieceColor::Black)), None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, Some((PieceType::Rook, PieceColor::White)), None, None, None, None, None, None],
+      ];
+      let mut board = Board::default();
+      board.set_board(custom_board);
+
+      let mut right_positions = vec![
+      vec![1, 7], 
+      vec![2, 6], 
+      vec![3, 3], 
+      vec![3, 5], 
+      vec![4, 1], 
+      vec![4, 2], 
+      vec![4, 3], 
+      vec![4, 5], 
+      vec![4, 6], 
+      vec![4, 7], 
+      vec![5, 3], 
+      vec![5, 4], 
+      vec![5, 5], 
+      vec![6, 2], 
+      vec![6, 4], 
+      vec![7, 4]];
+      
+      right_positions.sort();
+
+
+      let mut positions = Queen::authorized_positions([4, 4], PieceColor::White, board.board);
+      positions.sort();
+
+      assert_eq!(right_positions, positions);
+  }
+}
