4df93c035cfc0750571f794bb88936cbd7d7fc0b
Jeromos Kovacs <iITsnot.me214@proton.me>
2024-03-14 15:52:27 +0100

    Revert "fix(takeback): fixes takeback when something was kicked; added logging to file; code cleanup"
    This reverts commit 0da334150351f781e2231cf07290121c7b217cdb.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use chess_tui::event::{Event, EventHandler};
 use chess_tui::handler::handle_key_events;
 use chess_tui::tui::Tui;
 use clap::Parser;
-use log::{info, warn, LevelFilter};
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
 use std::fs::{self, File};
@@ -24,38 +23,20 @@ struct Args {
 fn main() -> AppResult<()> {
     // Parse the cli arguments
     let args = Args::parse();
-    info!("args: {:?}", args);
 
-    // initializing logger
-    fern::Dispatch::new()
-        .format(|out, message, record| out.finish(format_args!("[{}] {}", record.level(), message)))
-        .level(LevelFilter::Info)
-        .chain(fern::log_file("chess-tui.log")?)
-        .apply()?;
-
-    let config_path = dirs::config_dir().unwrap().join("chess-tui").join("config");
-    info!("config path: {:?}", config_path);
+    let config_path = dirs::config_dir().unwrap().join("chess-tui");
 
     if !args.engine_path.is_empty() {
         if !config_path.exists() {
-            if !config_path.parent().unwrap().exists() {
-                fs::create_dir(config_path.parent().unwrap())?;
-                info!(
-                    "creating config path dir at {:?}",
-                    config_path.parent().unwrap()
-                );
-            }
             File::create(&config_path)?;
-            info!("created config path");
         }
 
         let mut file = File::create(&config_path)?;
         file.write_all(args.engine_path.as_bytes())?;
-        info!("written engine path to log file");
     }
 
     // Create an application.
-    let mut app = App::default();
+    let mut app = App::new();
 
     // We store the chess engine path if there is one
     if let Ok(content) = fs::read_to_string(config_path) {
@@ -65,8 +46,7 @@ fn main() -> AppResult<()> {
             app.chess_engine_path = Some(content)
         }
     } else {
-        warn!("error reading the config file or the file does not exist");
-        println!("Error reading the config file or the file does not exist");
+        println!("Error reading the file or the file does not exist");
     }
 
     // Initialize the terminal user interface.
@@ -85,12 +65,12 @@ fn main() -> AppResult<()> {
         match tui.events.next()? {
             Event::Tick => app.tick(),
             Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
-            Event::Mouse(_) | Event::Resize(_, _) => {}
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
         }
     }
 
     // Exit the user interface.
     tui.exit()?;
-    info!("exiting app");
     Ok(())
 }
