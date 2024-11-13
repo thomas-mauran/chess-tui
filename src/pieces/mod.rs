89afcd4a69b3575f65de482055e74e662ff3f058
Thomas Mauran <thomasmauran@yahoo.com>
2024-11-13 19:19:59 +0100

    fix: code not running derive Eq for pieceType
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- src/pieces/mod.rs
+++ src/pieces/mod.rs
@@ -12,7 +12,7 @@ pub mod pawn;
 pub mod queen;
 pub mod rook;
 
-#[derive(Debug, Copy, Clone, PartialEq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Hash, Eq)]
 pub enum PieceType {
     Pawn,
     Rook,
