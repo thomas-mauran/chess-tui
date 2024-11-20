95757274573544f29ee098d06bce2e37cbcf42fb
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2024-11-20 18:37:34 +0100

    refactor: move tests (#103)
    * refactor: move tests
    
    ---------
    
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- src/utils.rs
+++ src/utils.rs
@@ -251,31 +251,3 @@ pub fn get_cell_paragraph<'a>(
 pub fn invert_position(coord: &Coord) -> Coord {
     Coord::new(7 - coord.row, 7 - coord.col)
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::utils::{convert_notation_into_position, convert_position_into_notation};
-
-    #[test]
-    fn convert_position_into_notation_1() {
-        assert_eq!(convert_position_into_notation("7152"), "b1-c3")
-    }
-
-    #[test]
-    fn convert_position_into_notation_2() {
-        assert_eq!(convert_position_into_notation("0257"), "c8-h3")
-    }
-
-    #[test]
-    fn convert_notation_into_position_1() {
-        assert_eq!(convert_notation_into_position("c8b7"), "0211")
-    }
-    #[test]
-    fn convert_notation_into_position_2() {
-        assert_eq!(convert_notation_into_position("g7h8"), "1607")
-    }
-    #[test]
-    fn convert_notation_into_position_3() {
-        assert_eq!(convert_notation_into_position("g1f3"), "7655")
-    }
-}
