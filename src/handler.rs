3de5ffdd9947a3bc2bcc3e7a93af3a7ce7d1bffb
Raoni Silvestre <raoni.silvestre@hotmail.com>
2025-06-05 11:09:58 -0300

    feat: refactor handler by pages

--- src/handler.rs
+++ src/handler.rs
@@ -27,216 +27,217 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
     }
 
     if app.current_popup == Some(Popups::EnterHostIP) {
-        if key_event.kind == KeyEventKind::Press {
-            match key_event.code {
-                KeyCode::Enter => {
-                    app.game.ui.prompt.submit_message();
-                    if app.current_page == Pages::Multiplayer {
-                        app.host_ip = Some(app.game.ui.prompt.message.clone());
-                    }
-                    app.current_popup = None;
-                }
-                KeyCode::Char(to_insert) => app.game.ui.prompt.enter_char(to_insert),
-                KeyCode::Backspace => app.game.ui.prompt.delete_char(),
-                KeyCode::Left => app.game.ui.prompt.move_cursor_left(),
-                KeyCode::Right => app.game.ui.prompt.move_cursor_right(),
-                KeyCode::Esc => {
-                    app.current_popup = None;
-                    if app.current_page == Pages::Multiplayer {
-                        app.hosting = None;
-                        app.selected_color = None;
-                        app.menu_cursor = 0;
-                    }
-                    app.current_page = Pages::Home;
-                }
-                _ => {}
-            }
-        }
-    } else {
         match key_event.code {
-            // Exit application on `q`
-            KeyCode::Char('q') => {
-                app.quit();
+            KeyCode::Enter => {
+                app.game.ui.prompt.submit_message();
+                if app.current_page == Pages::Multiplayer {
+                    app.host_ip = Some(app.game.ui.prompt.message.clone());
+                }
+                app.current_popup = None;
             }
-            // Exit application on `Ctrl-C`
-            KeyCode::Char('c' | 'C') => {
-                if key_event.modifiers == KeyModifiers::CONTROL {
-                    app.quit();
-                }
-            }
-            // Counter handlers
-            // Counter handlers
-            KeyCode::Right | KeyCode::Char('l') => {
-                if (app.current_page == Pages::Multiplayer
-                    && (app.hosting.is_none() || app.selected_color.is_none()))
-                    || (app.current_page == Pages::Bot && app.selected_color.is_none())
-                {
-                    app.menu_cursor_right(2);
-                } else if app.game.game_state == GameState::Promotion {
-                    app.game.ui.cursor_right_promotion();
-                } else if !(app.game.game_state == GameState::Checkmate)
-                    && !(app.game.game_state == GameState::Draw)
-                {
-                    let authorized_positions = app.game.game_board.get_authorized_positions(
-                        app.game.player_turn,
-                        app.game.ui.selected_coordinates,
-                    );
-                    app.game.ui.cursor_right(authorized_positions);
-                }
-            }
-
-            KeyCode::Left | KeyCode::Char('h') => {
-                if (app.current_page == Pages::Multiplayer
-                    && (app.hosting.is_none() || app.selected_color.is_none()))
-                    || (app.current_page == Pages::Bot && app.selected_color.is_none())
-                {
-                    app.menu_cursor_left(2);
-                } else if app.game.game_state == GameState::Promotion {
-                    app.game.ui.cursor_left_promotion();
-                } else if !(app.game.game_state == GameState::Checkmate)
-                    && !(app.game.game_state == GameState::Draw)
-                {
-                    let authorized_positions = app.game.game_board.get_authorized_positions(
-                        app.game.player_turn,
-                        app.game.ui.selected_coordinates,
-                    );
-
-                    app.game.ui.cursor_left(authorized_positions);
-                }
-            }
-            KeyCode::Up | KeyCode::Char('k') => {
-                if app.current_page == Pages::Home {
-                    app.menu_cursor_up(Pages::variant_count() as u8);
-                } else if !(app.game.game_state == GameState::Checkmate)
-                    && !(app.game.game_state == GameState::Draw)
-                    && !(app.game.game_state == GameState::Promotion)
-                {
-                    let authorized_positions = app.game.game_board.get_authorized_positions(
-                        app.game.player_turn,
-                        app.game.ui.selected_coordinates,
-                    );
-                    app.game.ui.cursor_up(authorized_positions);
-                }
-            }
-            KeyCode::Down | KeyCode::Char('j') => {
-                if app.current_page == Pages::Home {
-                    app.menu_cursor_down(Pages::variant_count() as u8);
-                } else if !(app.game.game_state == GameState::Checkmate)
-                    && !(app.game.game_state == GameState::Draw)
-                    && !(app.game.game_state == GameState::Promotion)
-                {
-                    let authorized_positions = app.game.game_board.get_authorized_positions(
-                        app.game.player_turn,
-                        app.game.ui.selected_coordinates,
-                    );
-
-                    app.game.ui.cursor_down(authorized_positions);
-                }
-            }
-            KeyCode::Char(' ') | KeyCode::Enter => match app.current_page {
-                Pages::Home => {
-                    app.menu_select();
-                }
-                Pages::Bot => {
-                    if app.selected_color.is_none() {
-                        app.color_selection();
-                        app.bot_setup();
-                    } else {
-                        app.game.handle_cell_click();
-                    }
-                }
-                Pages::Multiplayer => {
-                    if app.hosting.is_none() {
-                        app.hosting_selection();
-                    } else if app.selected_color.is_none() {
-                        if app.hosting.is_some() && app.hosting.unwrap() {
-                            app.color_selection();
-                        }
-                    } else {
-                        app.game.handle_cell_click();
-                    }
-                }
-                Pages::Credit => {
-                    app.current_page = Pages::Home;
-                }
-                _ => {
-                    app.game.handle_cell_click();
-                }
-            },
-            KeyCode::Char('?') => {
-                if app.current_page != Pages::Credit {
-                    app.toggle_help_popup();
-                }
-            }
-            KeyCode::Char('r') => {
-                // We can't restart the game if it's a multiplayer one
-                if app.game.opponent.is_none() {
-                    app.restart();
-                }
-            }
+            KeyCode::Char(to_insert) => app.game.ui.prompt.enter_char(to_insert),
+            KeyCode::Backspace => app.game.ui.prompt.delete_char(),
+            KeyCode::Left => app.game.ui.prompt.move_cursor_left(),
+            KeyCode::Right => app.game.ui.prompt.move_cursor_right(),
             KeyCode::Esc => {
-                match app.current_popup {
-                    Some(Popups::ColorSelection) => {
-                        app.current_popup = None;
-                        app.selected_color = None;
-                        app.hosting = None;
-                        app.current_page = Pages::Home;
-                        app.menu_cursor = 0;
-                    }
-                    Some(Popups::MultiplayerSelection) => {
-                        app.current_popup = None;
-                        app.selected_color = None;
-                        app.hosting = None;
-                        app.current_page = Pages::Home;
-                        app.menu_cursor = 0;
-                    }
-                    Some(Popups::WaitingForOpponentToJoin) => {
-                        app.current_popup = None;
-                        app.selected_color = None;
-                        app.hosting = None;
-                        app.current_page = Pages::Home;
-                        app.menu_cursor = 0;
-                    }
-                    Some(Popups::Help) => {
-                        app.current_popup = None;
-                    }
-                    _ => {}
-                }
-
-                if app.current_page == Pages::Credit {
-                    app.current_page = Pages::Home;
-                }
-
-                app.game.ui.unselect_cell();
-            }
-            KeyCode::Char('b') => {
-                let display_mode = app.game.ui.display_mode;
-                app.selected_color = None;
-                if app.game.bot.is_some() {
-                    app.game.bot = None;
-                }
-                if app.game.opponent.is_some() {
-                    app.game
-                        .opponent
-                        .as_mut()
-                        .unwrap()
-                        .send_end_game_to_server();
-                    app.game.opponent = None;
+                app.current_popup = None;
+                if app.current_page == Pages::Multiplayer {
                     app.hosting = None;
-                    app.host_ip = None;
+                    app.selected_color = None;
+                    app.menu_cursor = 0;
                 }
-
-                app.go_to_home();
-                app.game.game_board.reset();
-                app.game.ui.reset();
-                app.game.ui.display_mode = display_mode;
+                app.current_page = Pages::Home;
             }
-            // Other handlers you could add here.
             _ => {}
         }
+
+        return Ok(());
+    }
+
+    match &app.current_page {
+        Pages::Home => handle_home_page_events(app, key_event),
+        Pages::Solo => handle_solo_page_events(app, key_event),
+        Pages::Multiplayer => handle_multiplayer_page_events(app, key_event),
+        Pages::Bot => handle_bot_page_events(app, key_event),
+        Pages::Credit => handle_credit_page_events(app, key_event),
     }
 
     Ok(())
+}
+
+fn handle_home_page_events(app: &mut App, key_event: KeyEvent) {
+    match key_event.code {
+        // Exit application on `q`
+        KeyCode::Char('q') => app.quit(),
+        KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => app.quit(),
+        KeyCode::Up | KeyCode::Char('k') => app.menu_cursor_up(Pages::variant_count() as u8),
+        KeyCode::Down | KeyCode::Char('j') => app.menu_cursor_down(Pages::variant_count() as u8),
+        KeyCode::Char(' ') | KeyCode::Enter => app.menu_select(),
+        KeyCode::Char('?') => app.toggle_help_popup(),
+        KeyCode::Esc if Some(Popups::Help) == app.current_popup => app.toggle_help_popup(),
+        _ => (),
+    }
+}
+
+fn handle_solo_page_events(app: &mut App, key_event: KeyEvent) {
+    let is_playing = app.game.game_state == GameState::Playing;
+
+    match key_event.code {
+        KeyCode::Char('q') => app.quit(),
+        KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => app.quit(),
+
+        KeyCode::Up | KeyCode::Char('k') if is_playing => go_up_in_game(app),
+        KeyCode::Down | KeyCode::Char('j') if is_playing => go_down_in_game(app),
+
+        KeyCode::Right | KeyCode::Char('l') => match app.game.game_state {
+            GameState::Promotion => app.game.ui.cursor_right_promotion(),
+            GameState::Playing => go_right_in_game(app),
+            _ => (),
+        },
+        KeyCode::Left | KeyCode::Char('h') => match app.game.game_state {
+            GameState::Promotion => app.game.ui.cursor_left_promotion(),
+            GameState::Playing => go_left_in_game(app),
+            _ => (),
+        },
+
+        KeyCode::Char(' ') | KeyCode::Enter => app.game.handle_cell_click(),
+
+        KeyCode::Char('?') => app.toggle_help_popup(),
+        KeyCode::Char('r') => app.restart(),
+
+        KeyCode::Char('b') => {
+            let display_mode = app.game.ui.display_mode;
+            app.selected_color = None;
+            app.game.bot = None;
+            app.go_to_home();
+            app.game.game_board.reset();
+            app.game.ui.reset();
+            app.game.ui.display_mode = display_mode;
+        }
+        _ => (),
+    }
+}
+
+fn handle_multiplayer_page_events(app: &mut App, key_event: KeyEvent) {
+    let is_selecting = app.hosting == None || app.selected_color == None;
+    let should_select_color = app.selected_color == None && app.hosting == Some(true);
+    let should_select_hosting = app.hosting == None;
+    let is_playing = app.game.game_state == GameState::Playing;
+
+    match key_event.code {
+        KeyCode::Char('q') => app.quit(),
+        KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => app.quit(),
+
+        KeyCode::Right | KeyCode::Char('l') if is_selecting => app.menu_cursor_right(2),
+        KeyCode::Right | KeyCode::Char('l') => match app.game.game_state {
+            GameState::Promotion => app.game.ui.cursor_right_promotion(),
+            GameState::Playing => go_right_in_game(app),
+            _ => (),
+        },
+
+        KeyCode::Left | KeyCode::Char('h') if is_selecting => app.menu_cursor_left(2),
+        KeyCode::Left | KeyCode::Char('h') => match app.game.game_state {
+            GameState::Promotion => app.game.ui.cursor_left_promotion(),
+            GameState::Playing => go_left_in_game(app),
+            _ => (),
+        },
+        KeyCode::Up | KeyCode::Char('k') if is_playing => go_up_in_game(app),
+        KeyCode::Down | KeyCode::Char('j') if is_playing => go_down_in_game(app),
+
+        KeyCode::Char(' ') | KeyCode::Enter if should_select_hosting => app.hosting_selection(),
+        KeyCode::Char(' ') | KeyCode::Enter if should_select_color => app.color_selection(),
+        KeyCode::Char(' ') | KeyCode::Enter => app.game.handle_cell_click(),
+
+        KeyCode::Char('?') => app.toggle_help_popup(),
+
+        KeyCode::Char('b') => {
+            let display_mode = app.game.ui.display_mode;
+            app.selected_color = None;
+            app.game.bot = None;
+
+            if let Some(opponent) = app.game.opponent.as_mut() {
+                opponent.send_end_game_to_server();
+                app.game.opponent = None;
+                app.hosting = None;
+                app.host_ip = None;
+            }
+
+            app.go_to_home();
+            app.game.game_board.reset();
+            app.game.ui.reset();
+            app.game.ui.display_mode = display_mode;
+        }
+
+        // Continue from here to add more commands for Multiplayer
+        _ => (),
+    }
+}
+
+// Probably this four functions should be a method for app
+fn go_left_in_game(app: &mut App) {
+    let authorized_positions = app
+        .game
+        .game_board
+        .get_authorized_positions(app.game.player_turn, app.game.ui.selected_coordinates);
+    app.game.ui.cursor_left(authorized_positions);
+}
+
+fn go_right_in_game(app: &mut App) {
+    let authorized_positions = app
+        .game
+        .game_board
+        .get_authorized_positions(app.game.player_turn, app.game.ui.selected_coordinates);
+    app.game.ui.cursor_right(authorized_positions);
+}
+
+fn go_up_in_game(app: &mut App) {
+    let authorized_positions = app
+        .game
+        .game_board
+        .get_authorized_positions(app.game.player_turn, app.game.ui.selected_coordinates);
+    app.game.ui.cursor_up(authorized_positions);
+}
+
+fn go_down_in_game(app: &mut App) {
+    let authorized_positions = app
+        .game
+        .game_board
+        .get_authorized_positions(app.game.player_turn, app.game.ui.selected_coordinates);
+    app.game.ui.cursor_down(authorized_positions);
+}
+
+// NOT TESTED (didn't try with a bot)
+fn handle_bot_page_events(app: &mut App, key_event: KeyEvent) {
+    let is_selecting = app.selected_color == None;
+
+    match key_event.code {
+        KeyCode::Char('q') => app.quit(),
+        KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => app.quit(),
+
+        KeyCode::Left | KeyCode::Char('h') if is_selecting => app.menu_cursor_left(2),
+        KeyCode::Right | KeyCode::Char('l') if is_selecting => app.menu_cursor_right(2),
+
+        KeyCode::Enter | KeyCode::Char(' ') if is_selecting => {
+            app.color_selection();
+            app.bot_setup();
+        }
+        KeyCode::Enter | KeyCode::Char(' ') => app.game.handle_cell_click(),
+
+        _ => (),
+    }
+}
+
+// OK
+fn handle_credit_page_events(app: &mut App, key_event: KeyEvent) {
+    match key_event.code {
+        // Exit application on `q` or `Cntrl + C`
+        KeyCode::Char('q') => app.quit(),
+        KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => app.quit(),
+
+        KeyCode::Char(' ') | KeyCode::Esc | KeyCode::Enter => app.toggle_credit_popup(),
+
+        _ => (),
+    }
 }
 
 pub fn handle_mouse_events(mouse_event: MouseEvent, app: &mut App) -> AppResult<()> {
