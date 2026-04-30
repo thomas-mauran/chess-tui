5a303cb142184e34a597cc3b6b865f260576f24b
Thomas Mauran <thomasmauran@yahoo.com>
2026-04-30 22:59:44 +0800

    chore: fix docusaurus doc and warnings
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   i18n: {
     defaultLocale: 'en',
@@ -130,6 +129,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
   themes: ['@docusaurus/theme-mermaid'],
 };
